aec7952f7f2bda57100f5b6136022bf58e149797
gladkill <gladkillmusic@gmail.com>
2020-03-27 16:25:38 -0700

    several routes built, up to step 7. template/static updated

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Flask app for adopt app."""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,12 +8,14 @@ from models import db, connect_db, Pet
 
 from forms import AddPetForm
 
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "abcdef"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
 
 connect_db(app)
 db.create_all()
@@ -32,7 +34,38 @@ def index():
     return render_template('pet-listing.html', pets=pets)
 
 
-@app.route('/add')
-def 
+@app.route('/add', methods=["POST", "GET"])
+def add_pet():
 
-gladkillmusic
+    form = AddPetForm()
+
+
+    if form.validate_on_submit():
+        pet_name = form.name.data
+        species = form.species.data
+        
+        validate_species(form,species)
+
+        photo_url = form.photo_url.data or None
+        age = form.age.data
+        notes = form.notes.data or None
+
+        new_pet = Pet(name=pet_name,
+                      species=species,
+                      photo_url=photo_url,
+                      age=age,
+                      notes=notes
+                      )
+        db.session.add(new_pet)
+        db.session.commit()
+    else:
+        return render_template("add-pet.html",
+                               form=form)
+
+    return redirect("/")
+
+@app.route('/<int:pet_id>')
+def pet_details(pet_id):
+    current_pet = Pet.query.get_or_404(pet_id)
+    
+    return render_template('pet-details.html', pet=current_pet)
--- forms.py
+++ forms.py
@@ -2,16 +2,20 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, FloatField, SelectField
-from wtforms.validators import InputRequired, Optional, Email
+from wtforms.validators import InputRequired, Optional, Email, url
 
 
 class AddPetForm(FlaskForm):
 
     name = StringField("Name", validators=[InputRequired()])
-    species = StringField("Species", validators=[InputRequired()])
-    photo_url = StringField("Photo Url", validators=[Optional()])
+    species = SelectField("Species", 
+                        choices=[('dog','Dog'), ('cat','Cat'),
+                        ('porcupine','Porcupine')])
+    photo_url = StringField("Photo Url", validators=[Optional(),url()])
     age = SelectField("Age",
                       choices=[('baby', 'Baby'), ('young', 'Young'),
                                ('adult', 'Adult'), ('senior', 'Senior')]
                       )
     notes = StringField("Notes", validators=[Optional()])
+
+
